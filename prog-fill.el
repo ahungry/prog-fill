556a7c6eece4336d4e015cd1b2ac1fd7590f2d6e
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2018-01-28 20:21:04 +0100

    Update headers for easier package.el browsing
    - Update the URL so the user can easily go to the correct website.
    - Add some of the documentation from README.md (slightly updated) to
      make it easier for the user to see what the mode does when browsing to
      the package with package.el.

--- prog-fill.el
+++ prog-fill.el
@@ -4,7 +4,7 @@
 
 ;; Author: Matthew Carter <m@ahungry.com>
 ;; Maintainer: Matthew Carter <m@ahungry.com>
-;; URL: https://github.com/ahungry/color-theme-ahungry
+;; URL: https://github.com/ahungry/prog-fill
 ;; Version: 0.0.1
 ;; Keywords: ahungry convenience c formatting editing
 ;; Package-Requires: ((emacs "25.1") (cl-lib "0.6.1"))
@@ -28,7 +28,42 @@
 
 ;;; Commentary:
 
-;; Make
+;; Have you ever hated having really long chained method calls in heavy OOP
+;; languages? or just enjoy avoiding hitting the 80 column margin in any others?
+;;
+;; If so, this is the mode for you!
+;;
+;; Examples:
+;;
+;; Imagine coming across this mess - who wants to try to mentally parse that
+;; out?
+;;
+;;     $this->setBlub((Factory::get('some-thing', 'with-args'))->inner())->withChained(1, 2, 3);
+;;
+;; Hit M-q on the line (after binding it to `prog-fill' for that mode) and it
+;; becomes:
+;;
+;;     $this
+;;         ->setBlub(
+;;             (Factory::get(
+;;                 'some-thing',
+;;                 'with-args'))
+;;             ->inner())
+;;         ->withChained(
+;;             1,
+;;             2,
+;;             3);
+;;
+;; Or maybe you've got a crazy javasript promise chain you're working on?
+;;
+;;     superagent.get(someUrl).then(response => response.body).catch(reason => console.log(reason))
+;;
+;; Again, press M-q on the line and it becomes:
+;;
+;;     superagent.get(someUrl)
+;;       .then(response => response.body)
+;;       .catch(reason => console.log(reason))
+;;
 
 ;;; Code:
 (require 'cl-lib)
